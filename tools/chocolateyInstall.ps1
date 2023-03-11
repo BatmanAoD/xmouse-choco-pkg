9db051b9ab887b88a8aed4cee28c446ec08c797d
Kyle J Strand <batmanaod@gmail.com>
2023-03-11 12:11:18 -0700

    try to install a Start menu shortcut

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -9,4 +9,11 @@ $packageArgs = @{
 Get-ChocolateyUnzip @packageArgs
 
 # Create `...exe.gui` if it does not already exist
-$null >> "$toolsPath\X-Mouse Controls.exe.gui"
+$programPath = Join-Path $toolsPath "X-Mouse Controls.exe"
+$null >> "$programPath.gui"
+
+# Install a Start menu shortcut
+$shotcutName = "X-Mouse Controls.lnk"
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutFilePath = Join-Path $programs $shortcutName 
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $programPath
